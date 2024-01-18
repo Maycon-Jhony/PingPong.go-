9bc78ab2c654547403e50fd363adde9995f936fa
Maycon Jhony <152757610+Maycon-Jhony@users.noreply.github.com>
2024-01-17 22:33:54 -0300

    Add files via upload

---
+++ pingpong.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func pingar(c chan string) {
+	for i := 0; ; i++ {
+		c <- "ping"
+	}
+}
+func pong(c chan string) {
+	for i := 0; ; i++ {
+		c <- "pong"
+	}
+}
+func imprimir(c chan string) {
+	for {
+		msg := <-c
+		fmt.Println(msg)
+		time.Sleep(time.Second * 1)
+	}
+}
+func main() {
+	var c chan string = make(chan string)
+
+	go pingar(c)
+	go imprimir(c)
+	go pong(c)
+
+	var entrada string
+	fmt.Scanln(&entrada)
+}
